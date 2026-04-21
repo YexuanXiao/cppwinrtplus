a41c57b6ad83762a7a64ffcb223b33918588dfaa
Yexuan Xiao <bizwen@nykz.org>
2026-04-22 05:55:51 +0800

    Port wil::resume_foreground (#62)

---
+++ strings/base_coroutine_winui.h
@@ -0,0 +1,11 @@
+
+namespace winrt
+{
+    template <>
+    struct dispatcher_traits<Microsoft::UI::Dispatching::DispatcherQueue>
+    {
+        using Priority = Microsoft::UI::Dispatching::DispatcherQueuePriority;
+        using Handler = Microsoft::UI::Dispatching::DispatcherQueueHandler;
+        using Scheduler = decltype([](auto const& d, auto const& p, auto const& h) { d.TryEnqueue(p, h); });
+    };
+}
